b38735a059ecfa8668a112c0cd9101c173bb0401
Vishruti Patil <vishruti2908@gmail.com>
2023-03-23 14:01:48 +0530

    Created Maven Project for JUnit Testing

---
+++ src/main/java/UserRegistration.java
@@ -0,0 +1,5 @@
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("User Registration Validation using Regex and implementation for JUnit Testing");
+    }
+}
