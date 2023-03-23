a058aa9366a2ac3ffbb7dfccea8e9c60edc21578
Vishruti Patil <vishruti2908@gmail.com>
2023-03-23 18:43:59 +0530

    test mathod for mobile number validation added

--- src/main/java/Regex/JUnitTesting/UserRegistration.java
+++ src/main/java/Regex/JUnitTesting/UserRegistration.java
@@ -5,19 +5,17 @@ import java.util.Scanner;
 
 public class UserRegistration {
     public static void main(String[] args) {
-                Scanner scanner = new Scanner(System.in);
-                System.out.print("Enter your email address: ");
-                String email = scanner.nextLine();
-
-                // Define regex pattern for email validation
-                Pattern pattern = Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$");
-
-                // Match user input against regex pattern
-                Matcher matcher = pattern.matcher(email);
-                if (matcher.matches()) {
-                    System.out.println("Valid email");
-                } else {
-                    System.out.println("Invalid email");
-                }
-            }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your mobile number : ");
+        String mobileNumber = scanner.nextLine();
+        //Regex Pattern for pre-defined mobile number
+        Pattern pattern = Pattern.compile("^\\d{2} \\d{10}$");
+        // Match user input against regex pattern
+        Matcher matcher = pattern.matcher(mobileNumber);
+        if (matcher.matches()) {
+            System.out.println("Valid mobile number");
+        } else {
+            System.out.println("Invalid mobile number");
+        }
+    }
 }
